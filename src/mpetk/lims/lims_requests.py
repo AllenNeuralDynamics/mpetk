71867456521de36fd9f42611f133045f668b37be
ariellel <ariellel@alleninstitute.org>
2021-06-03 15:22:17 -0700

    adding tests and fixing post exception

--- src/mpetk/lims/lims_requests.py
+++ src/mpetk/lims/lims_requests.py
@@ -50,10 +50,8 @@ def post(url, data, *args, timeout=None):
         t1 = datetime.datetime.now()
         response = requests.post(_request, json=data, timeout=timeout)
         t_delta = datetime.datetime.now() - t1
-    except exceptions.LIMSUnavailableError:
-        raise exceptions.LIMSUnavailableError(f"Post request to {_request} failed with no response.")
     except requests.exceptions.ConnectionError:
-        raise requests.exceptions.ConnectionError(f"Post request to {_request} timed out trying to post")
+        raise exceptions.LIMSUnavailableError(f"Post request to {_request} failed with no response.")
     logging.lims(f'LIMS POST: {_request}, status code: {response.status_code}, {t_delta.total_seconds():.2f} seconds',
                  extra={'weblog': True})
     logging.info(f'POST data: {pformat(json.dumps(data))}')
