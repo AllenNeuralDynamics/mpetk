76f83ce82555d91567af35658b54728593fb30b7
ariellel <ariellel@alleninstitute.org>
2021-06-09 15:39:28 -0700

    bumping version

--- src/mpetk/mpeconfig/python_3/__init__.py
+++ src/mpetk/mpeconfig/python_3/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """ben sutton"""
 __email__ = "ben.sutton@alleninstitute.org"
-__version__ = '0.2.0.dev3'
+__version__ = '0.2.0.dev4'
 
 from .config_server import ConfigServer  # noqa
 from .mpeconfig import fetch_configuration
