b63440336f1f749cccfd229dae82efa996beedf2
rossh <rossh@alleninstitute.org>
2022-07-15 11:30:11 -0700

    build:  bumped to 0.4.1

--- src/mpetk/lims/__init__.py
+++ src/mpetk/lims/__init__.py
@@ -4,7 +4,7 @@ from mpetk import mpeconfig
 
 __author__ = """ross hytnen"""
 __email__ = "rossh@alleninstitute.org"
-__version__ = '0.4.1.dev0'
+__version__ = '0.4.1'
 
 
 from . import lims_requests
