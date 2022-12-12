fec4368c5826f04e3ec4de9b908faeea707fc6ff
ross hytnen <rossh@alleninstitute.org>
2022-12-12 09:56:16 -0800

    updated version

--- build.py
+++ build.py
@@ -20,7 +20,7 @@ default_task = ['install_dependencies',
 
 # project meta
 name = 'mpetk'
-version = '0.4.1'
+version = '0.4.2'
 summary = 'configuration tools for mpe projects'
 description = __doc__
 authors = (Author('ross hytnen', 'rossh@alleninstitute.org'),
