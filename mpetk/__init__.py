e5c9db4e4d27ebbfed5d01e1b08619f2772f4408
Ross Hytnen <rossh@alleninstitute.org>
2024-10-31 11:00:08 -0700

    adding kp functions to init

--- mpetk/__init__.py
+++ mpetk/__init__.py
@@ -1,9 +1,28 @@
 from . import mpeconfig
-# from . import lims as limstk
-
 from . import piddl
 from . import zro
-# from . import aibsmw
 from . import teams
+from pykeepass import PyKeePass
 
 __version__ = '0.5.2.dev2'
+
+
+def kp_db(key_file=r'//allen/aibs/mpe/keepass/sipe_sw_passwords.kdbx',
+          db_file=r'c:\ProgramData\AIBS_MPE\.secrets\sipe_sw_passwords.kdbx'):
+    from pykeepass import PyKeePass
+    return PyKeePass(db_file, keyfile=key_file)
+
+
+def kp_query(key_file=r'//allen/aibs/mpe/keepass/sipe_sw_passwords.kdbx',
+             db_file=r'c:\ProgramData\AIBS_MPE\.secrets\sipe_sw_passwords.kdbx',
+             query: dict[str, any] | None =None):
+
+
+    keepass = kp_db(key_file, db_file)
+    query = query or dict()
+
+    # Query by group requires a group object rather than group name as str.
+    if group_name := query.get("group"):
+        query["group"] = keepass.find_groups(name=group_name, first=True)
+
+    return keepass.find_entries(**query, first=True)
